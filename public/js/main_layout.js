a676e59141b12e0ea3489ec817072028ef9afb49
Mawiusfp <kevincarpetano@proton.me>
2026-02-09 15:15:04 +0100

    peticion asincrona al cargar

--- public/js/main_layout.js
+++ public/js/main_layout.js
@@ -1,3 +1,14 @@
+
+function load_document() {
+    fetch('json/submenu.json')
+        .then(response => response.json()
+        .then(data => {
+            console.log(data)
+        })
+    );
+ }
+
 document.addEventListener('DOMContentLoaded', () => {
-    alert("This is the js for the template of all (most) pages");
+    console.log("This is the js for the template of all (most) pages");
+    load_document();   
 });
