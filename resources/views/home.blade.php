b4c83d9c9e45d850d61998e24c76e9d0a39e5364
Iria Nieto <irianietomontilla@gmail.com>
2026-02-17 13:11:27 +0100

    Botones arribita

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,35 +19,9 @@
 
                 <h1>Mi App de bicicletas</h1>
 
-                <nav class="main-navbar">
-                    <div>
-                        <label for="inicio">Inicio</label>
-                        <select name="inicio" id="inicio"></select>
-                    </div>
-
-                    <div>
-                        <label for="planes">Planes</label>
-                        <select name="planes" id="planes"></select>
-                    </div>
-
-                    <div>
-                        <label for="sesiones">Sesiones</label>
-                        <select name="sesiones" id="sesiones"></select>
-                    </div>
-
-                    <div>
-                        <label for="bloques">Bloques</label>
-                        <select name="bloques" id="bloques"></select>
-                    </div>
-
-                    <div>
-                        <label for="resultados">Resultados</label>
-                        <select name="resultados" id="resultados"></select>
-                    </div>
-                </nav>
+               
 
                 <div id="body">
-                    <!-- Aquí aparecerá el contenido al hacer click en los selects -->
                 </div>
 
             </div>
